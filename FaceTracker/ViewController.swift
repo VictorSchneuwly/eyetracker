de5b366e477cb055def7f7cfb0152544f6a9dd65
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-02-25 15:31:45 +0100

    Reformat using swiftformat

--- FaceTracker/ViewController.swift
+++ FaceTracker/ViewController.swift
@@ -5,69 +5,65 @@
 //  Created by Victor Schneuwly on 25.02.2024.
 //
 
-import UIKit
-import SpriteKit
 import ARKit
+import SpriteKit
+import UIKit
 
 class ViewController: UIViewController, ARSKViewDelegate {
-    
     @IBOutlet var sceneView: ARSKView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Set the view's delegate
         sceneView.delegate = self
-        
+
         // Show statistics such as fps and node count
         sceneView.showsFPS = true
         sceneView.showsNodeCount = true
-        
+
         // Load the SKScene from 'Scene.sks'
         if let scene = SKScene(fileNamed: "Scene") {
             sceneView.presentScene(scene)
         }
     }
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        
+
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
 
         // Run the view's session
         sceneView.session.run(configuration)
     }
-    
+
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        
+
         // Pause the view's session
         sceneView.session.pause()
     }
-    
+
     // MARK: - ARSKViewDelegate
-    
-    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
+
+    func view(_: ARSKView, nodeFor _: ARAnchor) -> SKNode? {
         // Create and configure a node for the anchor added to the view's session.
         let labelNode = SKLabelNode(text: "👾")
         labelNode.horizontalAlignmentMode = .center
         labelNode.verticalAlignmentMode = .center
-        return labelNode;
+        return labelNode
     }
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
+
+    func session(_: ARSession, didFailWithError _: Error) {
         // Present an error message to the user
-        
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
+
+    func sessionWasInterrupted(_: ARSession) {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
     }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
+
+    func sessionInterruptionEnded(_: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
     }
 }
--- FaceTrackerUITests/FaceTrackerUITestsLaunchTests.swift
+++ FaceTrackerUITests/FaceTrackerUITestsLaunchTests.swift
@@ -8,7 +8,6 @@
 import XCTest
 
 final class FaceTrackerUITestsLaunchTests: XCTestCase {
-
     override class var runsForEachTargetApplicationUIConfiguration: Bool {
         true
     }

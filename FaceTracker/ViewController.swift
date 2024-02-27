eb9086f2406e6745fa68b5263ac9eaa93d454204
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-02-27 11:22:00 +0100

    Retart session when back on the app

--- FaceTracker/ViewController.swift
+++ FaceTracker/ViewController.swift
@@ -111,7 +111,10 @@ extension ViewController: ARSKViewDelegate {
         // Inform the user that the session has been interrupted, for example, by presenting an overlay
     }
 
-    func sessionInterruptionEnded(_: ARSession) {
+    func sessionInterruptionEnded(_ session: ARSession) {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
+        sceneView.session.run(session.configuration!,
+                              options: [.resetTracking,
+                                        .removeExistingAnchors])
     }
 }

ac1bc376a96b1ff425a29ccfc3d611c8f5ec0477
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-04-12 14:28:10 +0200

    Update tests

--- EyeTrackerTests/EyeTrackerTests.swift
+++ EyeTrackerTests/EyeTrackerTests.swift
@@ -7,23 +7,37 @@
 
 import XCTest
 @testable import EyeTracker
+import SceneKit
+import SpriteKit
+import UIKit
+import XCTest
 
 final class EyeTrackerTests: XCTestCase {
+    // MARK: Test intersection
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testIntersectionWhenLookingAtCamera() {
+        let eyeTracker = AREyeTracker()
+        let eyePosition = simd_float3(0, 0, 0)
+        let eyeDirection = simd_float3(0, 0, -1)
+        let intersection = eyeTracker.intersectionWithPlane(eyePosition: eyePosition, eyeDirection: eyeDirection)
+        XCTAssertEqual(intersection, simd_float3(0, 0, 0))
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testIntersectionWithNonZeroOrigin() {
+        let eyeTracker = AREyeTracker()
+        let eyePosition = simd_float3(1, 1, 1) // Non-zero origin
+        let eyeDirection = simd_float3(0, 0, -1) // Looking straight ahead
+        let intersection = eyeTracker.intersectionWithPlane(eyePosition: eyePosition, eyeDirection: eyeDirection)
+        XCTAssertEqual(intersection, simd_float3(1, 1, 0)) // Expected intersection on the z=0 plane
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testParallelEyeDirectionAndPlane() {
+        let eyeTracker = AREyeTracker()
+        let eyePosition = simd_float3(0, 0, 1) // Above the z=0 plane
+        let eyeDirection = simd_float3(1, 0, 0) // Parallel to the z=0 plane
+        let intersection = eyeTracker.intersectionWithPlane(eyePosition: eyePosition, eyeDirection: eyeDirection)
+        XCTAssertNil(intersection) // Expect nil as the direction is parallel to the plane
+    }
     }
 
     func testPrint() {
@@ -37,4 +51,26 @@ final class EyeTrackerTests: XCTestCase {
         print("PPI: \(Device.ppi ?? -1)")
     }
 
+    // MARK: Test project
+
+    func testProjectForPointMiddleRight() {
+        // We assume we use an iPad Mini 6th
+
+        let width = 0.1954
+        let heigh = 0.1348
+
+        let topLeft = SCNVector3(width, 0, 0)
+        let screenSize = UIScreen.main.bounds
+        let size = CGSize(width: screenSize.width, height: screenSize.height)
+        let screen = AREyeTracker.Screen(size: size)
+
+        let eyeTracker = AREyeTracker()
+        let projection = eyeTracker.project(point: topLeft, on: screen)
+        let expected = CGPoint(
+            x: screen.size.width,
+            y: screen.size.height / 2
+        )
+
+        XCTAssertEqual(projection, expected)
+    }
 }

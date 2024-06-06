f22a4e569fd19e9a990175ac49e81c1e175fd7ac
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-06-06 13:57:56 +0200

    Update init file of calibrator folder

--- Notebooks/calibrators/__init__.py
+++ Notebooks/calibrators/__init__.py
@@ -1,5 +1,10 @@
 # __init__.py for calibrators directory
 
 from .average_offset_calibrator import AverageOffsetCalibrator
-from .calibration_utils import import_from_csv
+from .calibration_utils import (
+    import_from_csv,
+    import_from_dataframe,
+    calib_data_to_dataframe,
+)
 from .weighted_offset_calibrator import WeightedOffsetCalibrator
+from .linear_regression_calibrator import LinearRegressionCalibrator

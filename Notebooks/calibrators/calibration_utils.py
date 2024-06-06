c1282436313ebc8dc88fe662ced5666ffba06cd5
Victor Schneuwly <victor.schneuwly@epfl.ch>
2024-06-06 13:57:34 +0200

    Add more utils

--- Notebooks/calibrators/calibration_utils.py
+++ Notebooks/calibrators/calibration_utils.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 from typing import Tuple
 
+import pandas as pd
 import numpy as np
 
 
@@ -41,10 +42,10 @@ def import_from_csv(filename: str) -> [CalibrationData]:
         # we can remove the first line as it contains the headers
         lines.pop(0)
 
-        return [extract_calibration_data(line) for line in lines]
+        return [extract_calibration_data_from_line(line) for line in lines]
 
 
-def extract_calibration_data(line: str) -> CalibrationData:
+def extract_calibration_data_from_line(line: str) -> CalibrationData:
     # Extract the data from the line and return a CalibrationData object
     # we can split the line by comma
     data = line.split(",")
@@ -61,4 +62,91 @@ def extract_calibration_data(line: str) -> CalibrationData:
         right_eye_transform=np.array([float(x) for x in data[25:41]]).reshape((4, 4)),
         left_eye_transform=np.array([float(x) for x in data[41:57]]).reshape((4, 4)),
         look_at_point=(float(data[57]), float(data[58]), float(data[59])),
+    )
+
+
+def import_from_dataframe(df) -> [CalibrationData]:
+    # Extract the data from the DataFrame and return a list of CalibrationData objects
+    return df.apply(extract_calibration_data_from_row, axis=1).tolist()
+
+
+def extract_calibration_data_from_row(row) -> CalibrationData:
+    return CalibrationData(
+        username=row["username"],
+        device_name=row["deviceName"],
+        position=HeadPosition(row["position"]),
+        distance=PositionToScreen(row["distance"]),
+        timestamp=row["timestamp"],
+        target_point=(float(row["targetPointX"]), float(row["targetPointY"])),
+        gaze_point=(float(row["gazePointX"]), float(row["gazePointY"])),
+        face_transform=np.array(
+            row[
+                [
+                    f"faceTransform_{i}_{j}"
+                    for i in range(4)
+                    for j in ["x", "y", "z", "w"]
+                ]
+            ].tolist()
+        ).reshape((4, 4)),
+        right_eye_transform=np.array(
+            row[
+                [
+                    f"rightEyeTransform_{i}_{j}"
+                    for i in range(4)
+                    for j in ["x", "y", "z", "w"]
+                ]
+            ].tolist()
+        ).reshape((4, 4)),
+        left_eye_transform=np.array(
+            row[
+                [
+                    f"leftEyeTransform_{i}_{j}"
+                    for i in range(4)
+                    for j in ["x", "y", "z", "w"]
+                ]
+            ].tolist()
+        ).reshape((4, 4)),
+        look_at_point=(
+            float(row["lookAtPointX"]),
+            float(row["lookAtPointY"]),
+            float(row["lookAtPointZ"]),
+        ),
+    )
+
+
+def calib_data_to_dataframe(data):
+    # turn a list of CalibrationData objects into a DataFrame
+    return pd.DataFrame(
+        [
+            {
+                "username": d.username,
+                "deviceName": d.device_name,
+                "position": d.position.value,
+                "distance": d.distance.value,
+                "timestamp": d.timestamp,
+                "targetPointX": d.target_point[0],
+                "targetPointY": d.target_point[1],
+                "gazePointX": d.gaze_point[0],
+                "gazePointY": d.gaze_point[1],
+                **{
+                    f"faceTransform_{i}_{j}": d.face_transform[i, ji]
+                    for i in range(4)
+                    for (ji, j) in enumerate(["x", "y", "z", "w"])
+                },
+                **{
+                    f"rightEyeTransform_{i}_{j}": d.right_eye_transform[i, ji]
+                    for i in range(4)
+                    for (ji, j) in enumerate(["x", "y", "z", "w"])
+                },
+                **{
+                    f"leftEyeTransform_{i}_{j}": d.left_eye_transform[i, ji]
+                    for i in range(4)
+                    for (ji, j) in enumerate(["x", "y", "z", "w"])
+                },
+                "lookAtPointX": d.look_at_point[0],
+                "lookAtPointY": d.look_at_point[1],
+                "lookAtPointZ": d.look_at_point[2],
+            }
+            for d in data
+        ]
     )
